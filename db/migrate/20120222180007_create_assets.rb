ee56a5f41b95285796103d652dde8124bf064ac7
jhjguxin <864248765@qq.com>
2012-02-23 18:46:07 +0800

    fix some error

--- db/migrate/20120222180007_create_assets.rb
+++ db/migrate/20120222180007_create_assets.rb
@@ -2,7 +2,7 @@ class CreateAssets < ActiveRecord::Migration
   def change
     create_table :assets do |t|
       t.string :username
-      t.integer :url
+      t.string :url
       t.integer :item_id
       t.timestamps
     end
